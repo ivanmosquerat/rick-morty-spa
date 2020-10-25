37d565ee8dc5edbd3d2fb65dbf3d3344ba6f4f5e
Ivan Mosquera <ivanmosquerat13@gmail.com>
2020-10-26 00:30:47 +1100

    Templates created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,16 @@
- 
+ const Home = () => {
+     const view = `
+        <div class="Characters">
+            <article class="Character-item">
+                <a href="#/1/">
+                    <img src="images" alt="name"/>
+                    <h2>Name</h2>
+                </a>
+            </article>
+        </div>
+     `;
+
+     return view;
+ }
+
+ export default Home;
